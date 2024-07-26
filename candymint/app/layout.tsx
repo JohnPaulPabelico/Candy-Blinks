4d677aad9538af2fa91aca1afbb6525fbdf40694
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-07-26 10:15:51 +0800

    Initial setup

---
+++ candymint/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Candy Mint",
+  description: "A Candy Machine BLink Generator",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
