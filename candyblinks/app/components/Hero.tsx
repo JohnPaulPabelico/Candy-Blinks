bb79fbf657b822f91a68bbc145f2dd4e82ea75e2
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-18 18:53:09 +0800

    Added decor

--- candyblinks/app/components/Hero.tsx
+++ candyblinks/app/components/Hero.tsx
@@ -6,11 +6,25 @@ import { HeroHighlight, Highlight } from "./ui/hero-highlight";
 
 function Hero() {
   return (
-    <div>
-      <section className="flex items-center justify-center min-h-screen bg-neutral-950">
-        <div className="text-5xl font-bold dm-sans text-white max-w-96 leading-snug">
-          Deploy your NFT Campaigns with{" "}
-          <span className="text-red-400">Candy Blinks</span>
+    <div
+    // style={{
+    //   backgroundImage: `url(/Bg.webp)`,
+    //   backgroundSize: "cover",
+    //   backgroundPosition: "center",
+    //   overflow: "hidden",
+    // }}
+    >
+      <section className="flex items-center justify-center min-h-screen bg-gradient-to-t from-neutral-950 from-60%  to-pink-950 ">
+        <div>
+          <div className="text-5xl font-bold dm-sans text-white max-w-96 leading-snug">
+            Deploy your NFT Campaigns with{" "}
+            <span className="text-red-400">Candy Blinks</span>
+          </div>
+          <div>
+            <button className="mt-5 text-xl bg-red-400 hover:bg-red-500 text-white dm-sans font-bold py-2 px-4 rounded">
+              Get Started!
+            </button>
+          </div>
         </div>
         <div>
           <Image
