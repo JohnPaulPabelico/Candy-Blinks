b69f0da680ae03bc36fbd34be71796ff0d7b2b46
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-20 14:53:01 +0800

    Redo styling

--- candyblinks/app/settings/page.tsx
+++ candyblinks/app/settings/page.tsx
@@ -19,7 +19,7 @@ export default async function Dashboard() {
   return (
     <SignedIn>
       <NavBar />
-      <div className="min-h-dvh flex justify-center items-center bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-pink-950 from-10% via-neutral-950 via-70%  to-neutral-950">
+      <div className="min-h-dvh flex justify-center items-center bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-pink-950  to-neutral-950">
         <span className="text-2xl font-bold text-white mr-10">
           MAMA MO SETTINGS
         </span>
