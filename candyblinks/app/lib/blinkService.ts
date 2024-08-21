b0ef088263e5f7d715fb76016437e836f566fb1f
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-21 23:31:31 +0800

    add crud blink service

---
+++ candyblinks/app/lib/blinkService.ts
@@ -0,0 +1,95 @@
+// lib/blinkService.ts
+import supabase from "./supabaseClient";
+
+export const getBlinks = async (userId: string) => {
+  try {
+    const { data, error } = await supabase
+      .from("blinks")
+      .select("*")
+      .eq("clerk_user_id", userId);
+
+    if (error) throw error;
+
+    return data || [];
+  } catch (error) {
+    console.error("Failed to fetch blinks: ", error);
+    throw error;
+  }
+};
+
+export const deleteBlink = async (id: number) => {
+  try {
+    const { error } = await supabase.from("blinks").delete().eq("id", id);
+
+    if (error) throw error;
+
+    return id;
+  } catch (error) {
+    console.error("Failed to delete blink: ", error);
+    throw error;
+  }
+};
+
+export const createBlink = async (
+  candyMachineId: string,
+  title: string,
+  label: string,
+  iconUrl: string,
+  description: string,
+  userId: string,
+  currentTime: number
+) => {
+  try {
+    const { data, error } = await supabase
+      .from("blinks")
+      .insert({
+        candymachine_id: candyMachineId,
+        title: title,
+        label: label,
+        image_url: iconUrl,
+        description: description,
+        created_at: currentTime,
+        clerk_user_id: userId,
+      })
+      .select();
+
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error("Failed to create blink: ", error);
+    throw error;
+  }
+};
+
+export const updateBlink = async (
+  id: number,
+  candyMachineId: string,
+  title: string,
+  label: string,
+  iconUrl: string,
+  description: string,
+  currentTime: number
+) => {
+  try {
+    const { data, error } = await supabase
+      .from("blinks")
+      .update({
+        candymachine_id: candyMachineId,
+        title: title,
+        label: label,
+        image_url: iconUrl,
+        description: description,
+        updated_at: currentTime,
+      })
+      .eq("id", id)
+      .select();
+
+    if (error) throw error;
+
+    return data;
+  } catch (error) {
+    console.error("Failed to update blink: ", error);
+    throw error;
+  }
+};
