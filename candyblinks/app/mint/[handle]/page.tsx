c555a846e57c5e98d80d6dc3c2d2d4b5b8bb4bbd
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-28 10:37:23 +0800

    Quick OG Metadata test

--- candyblinks/app/mint/[handle]/page.tsx
+++ candyblinks/app/mint/[handle]/page.tsx
@@ -1,4 +1,3 @@
-"use client";
 import React, { useRef, useState } from "react";
 import Image from "next/image";
 import { SignedIn, useAuth } from "@clerk/nextjs";
@@ -11,37 +10,32 @@ import {
   mplCandyMachine,
 } from "@metaplex-foundation/mpl-candy-machine";
 import { clusterApiUrl } from "@solana/web3.js";
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
 import { useSupabaseClerkClient } from "../../lib/supabaseClerkClient";
+import { Metadata } from "next";
 
-const candyBlinkUrl = process.env.NEXT_PUBLIC_CANDYBLINK_URL || "";
+export const metadata: Metadata = {
+  title: "Candy Blinks",
+  description: "A Candy Machine BLink Generator",
+  openGraph: {
+    title: "CandyBlinks",
+    description: "A Candy Machine BLink Generator",
+    url: "https://candyblinks.fun/",
+    siteName: "CandyBlinks",
+    type: "website",
+    // images: [
+    //   {
+    //     url: "https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png",
+    //     secureUrl:
+    //       "https://raw.githubusercontent.com/gitdagray/my-blogposts/main/images/og-card.png",
+    //     width: 1200,
+    //     height: 630,
+    //     alt: "Preview image for Dave Gray",
+    //   },
+    // ],
+  },
+};
 
 export default function Dashboard() {
-  const [candyMachineId, setCandyMachineId] = useState("");
-  const [title, setTitle] = useState("");
-  const [label, setLabel] = useState("");
-  const [iconUrl, setIconUrl] = useState("");
-  const [description, setDescription] = useState("");
-  const [success, setSuccess] = useState(false);
-  const { userId } = useAuth();
-  const ref = useRef<HTMLFormElement>(null);
-
-  const client = useSupabaseClerkClient();
-
-  const [touchedInputs, setTouchedInputs] = useState<
-    Record<InputField, boolean>
-  >({
-    candyMachineId: false,
-    title: false,
-    label: false,
-    iconUrl: false,
-    description: false,
-  });
-
-  const handleBlur = (field: InputField) => {
-    setTouchedInputs((prev) => ({ ...prev, [field]: true }));
-  };
-
   const endpoint = clusterApiUrl("devnet");
 
   const Toast = Swal.mixin({
@@ -62,24 +56,15 @@ export default function Dashboard() {
     <SignedIn>
       <div className="min-h-dvh flex gap-5 justify-center items-center bg-[radial-gradient(circle_at_top,_var(--tw-gradient-stops))] from-pink-950 from-10%   to-neutral-950">
         <div>
-          <div className="p-5 bg-neutral-800 rounded-lg shadow-lg shadow-pink-900/50 max-w-[440px] border-pink-900 border-2">
-            {iconUrl ? (
-              <Image
-                src={iconUrl}
-                alt="icon"
-                width={400}
-                height={400}
-                className="rounded-md"
-              />
-            ) : (
-              <Image
-                src={"/CandyBlinks.png"}
-                alt="logo"
-                width={400}
-                height={400}
-                className="rounded-md"
-              />
-            )}
+          <div className="p-5 bg-neutral-800 rounded-lg shadow-lg shadow-pink-900/50 max-w-[440px] border-pink-900 border-2 m-3">
+            <Image
+              src={"/CandyBlinks.png"}
+              alt="logo"
+              width={400}
+              height={400}
+              className="rounded-md"
+            />
+
             <div className="mt-3">
               <div className="text-sm dm-sans text-neutral-400 flex items-center ">
                 <Image
@@ -92,23 +77,20 @@ export default function Dashboard() {
                 candyblinks.fun
               </div>
               <div className="mt-3 text-lg dm-sans text-white">
-                <div className="mt-3 text-lg dm-sans text-white">
-                  {title ? title : "Title"}
-                </div>
+                <div className="mt-3 text-lg dm-sans text-white">Title</div>
               </div>
               <div className="mt-1 text-sm dm-sans text-neutral-400 text-wrap w-full">
-                {description
-                  ? description
-                  : "Description - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."}
+                {" "}
+                Description - Lorem ipsum dolor sit amet, consectetur adipiscing
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+                aliqua.
               </div>
               <button className="mt-3 p-2 rounded-md bg-black hover:bg-neutral-950 transition duration-200 w-full text-white">
-                <span className="text-white font-bold">
-                  {label ? label : "Label"}
-                </span>
+                <span className="text-white font-bold">Label</span>
               </button>
             </div>
           </div>
-        </div>{" "}
+        </div>
       </div>
     </SignedIn>
   );
