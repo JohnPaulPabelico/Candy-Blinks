1fdf598f4f37e4fa97096a00eebaec76d7b301d7
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-28 22:26:45 +0800

    Added layout for metadata

---
+++ candyblinks/app/create/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import Page from "./page";
+
+export const metadata: Metadata = {
+  title: "Create a Candy Blink",
+  description:
+    "Create and manage your Candy Machine BLinks effortlessly with our intuitive platform. Simplify NFT distribution and minting with ease.",
+};
+
+export default Page;
---
+++ candyblinks/app/dashboard/layout.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+import Page from "./page";
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+  description:
+    "Create and manage your Candy Machine BLinks effortlessly with our intuitive platform. Simplify NFT distribution and minting with ease.",
+};
+
+export default Page;
