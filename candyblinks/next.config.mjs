7988301c446fa51de30dda44f33a857e1dd338dd
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-08-30 21:09:14 +0800

    fix: csp issues

--- candyblinks/next.config.mjs
+++ candyblinks/next.config.mjs
@@ -4,17 +4,6 @@ const nextConfig = {
   images: {
     domains: ["i.imgur.com", "imgur.com"], // Allow images from i.imgur.com
   },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      Object.defineProperty(config, "devtool", {
-        get() {
-          return "cheap-source-map";
-        },
-        set() {},
-      });
-    }
-    return config;
-  },
 };
 
 export default nextConfig;
