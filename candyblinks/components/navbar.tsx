f930d058bfe83ce4336b5f62b7755bc4aa0bad20
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-09-05 20:39:40 +0800

    Fix: navbar

--- candyblinks/components/navbar.tsx
+++ candyblinks/components/navbar.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import { useState, useEffect } from "react";
 import Link from "next/link";
@@ -6,9 +5,10 @@ import Link from "next/link";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 import React from "react";
+import { FaBars } from "react-icons/fa6";
+import { GrClose } from "react-icons/gr";
 
 export default function Navbar() {
-
   const [barState, setBarState] = useState(false);
   const [isTop, setIsTop] = useState(true);
 
@@ -22,27 +22,26 @@ export default function Navbar() {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
   return (
     <div
-      className={`flex items-center lg:fixed top-0 w-full  px-5 min-h-30 justify-between z-10 transition py-5 ${
-        barState ? "bg-slate-950" : "bg-white/1"
+      className={`flex items-center fixed top-0 w-full px-5 min-h-30 justify-between z-10 transition py-5 ${
+        barState ? "bg-slate-950" : ""
       } ${isTop ? "lg:bg-transparent" : "backdrop-blur-md shadow-lg "}`}
     >
       <div>
-        <span className="ml-5 text-3xl text-red-400 font-bold dm-sans lg:visible invisible">
+        <span className="lg:ml-5 text-3xl text-red-400 font-bold dm-sans ">
           Candy Blinks
         </span>
       </div>
       <div
-        className={`transition ease-out lg:static absolute  lg:min-h-fit min-h-screen lg:w-auto w-full top-[100%] left-0 py-10 lg:py-0  ${
-          barState ? "bg-slate-950" : "bg-transparent"
+        className={`transition ease-out lg:static absolute lg:min-h-fit min-h-screen lg:w-auto w-full top-[100%] left-0 py-10 lg:py-0  ${
+          barState ? "bg-slate-950" : "hidden lg:block"
         }`}
       >
-        <ul className={`flex-col lg:flex-row flex items-center  lg:gap-0`}>
-          <li className={`${barState ? "block" : "invisible lg:visible"}`}>
+        <ul className={`flex-col lg:flex-row flex items-center lg:gap-0`}>
+          <li className={`${barState ? "block" : "lg:block hidden"}`}>
             <Link
-              className={`lg:text-xl text-3xl  dm-sans rounded-full m-4 hover:text-slate-400 text-slate-100 transition duration-200 `}
+              className={`lg:text-xl text-3xl dm-sans m-4 hover:text-slate-400 text-slate-100 transition duration-200 `}
               href="#home"
               onClick={() => {
                 setBarState(false);
@@ -52,9 +51,9 @@ export default function Navbar() {
               Home
             </Link>
           </li>
-          <li className={`${barState ? "block" : "invisible lg:visible"}`}>
+          <li className={`${barState ? "block mt-8" : "lg:block hidden"}`}>
             <Link
-              className={` lg:text-xl text-3xl  dm-sans rounded-full m-4 hover:text-slate-400 text-slate-100 transition duration-200 `}
+              className={` lg:text-xl text-3xl  dm-sans m-4 hover:text-slate-400 text-slate-100 transition duration-200 `}
               href="#about"
               onClick={() => {
                 setBarState(false);
@@ -64,9 +63,9 @@ export default function Navbar() {
               About
             </Link>
           </li>
-          <li className={`${barState ? "block" : "invisible lg:visible"}`}>
+          <li className={`${barState ? "block mt-8" : "lg:block hidden"}`}>
             <Link
-              className={` lg:text-xl text-3xl  dm-sans rounded-full m-4 hover:text-slate-400 text-slate-100 transition duration-200`}
+              className={` lg:text-xl text-3xl  dm-sans m-4 hover:text-slate-400 text-slate-100 transition duration-200`}
               href="#explore"
               onClick={() => {
                 setBarState(false);
@@ -76,9 +75,9 @@ export default function Navbar() {
               Explore
             </Link>
           </li>
-          <li className={`${barState ? "block" : "invisible lg:visible"}`}>
+          <li className={`${barState ? "block mt-8" : "lg:block hidden"}`}>
             <Link
-              className={` lg:text-xl text-3xl  dm-sans rounded-full m-4 hover:text-slate-400 text-slate-100 transition duration-200`}
+              className={` lg:text-xl text-3xl  dm-sans m-4  hover:text-slate-400 text-slate-100 transition duration-200`}
               href="#contact"
               onClick={() => {
                 setBarState(false);
@@ -91,10 +90,10 @@ export default function Navbar() {
         </ul>
       </div>
       <div>
-        <div className={`${barState ? "block" : "invisible lg:visible"}`}>
+        <div className={`${barState ? "block" : "lg:block hidden"}`}>
           <div className="z-999 lg:block hidden ">
             <SignedOut>
-              <div>
+              <div className="flex gap-2 items-center">
                 <Link
                   className="text-xl bg-red-400 hover:bg-red-500 text-white dm-sans font-bold py-2 px-4 rounded duration-200 transition"
                   href="/dashboard"
@@ -116,8 +115,30 @@ export default function Navbar() {
           </div>
         </div>
       </div>
+      <div className="lg:hidden">
+        <ul className="flex items-center">
+          <li
+            className={`ml-5 m-3 text-2xl cursor-pointer hover:text-slate-200 text-slate-100 transition duration-200 
+              `}
+          >
+            {barState ? (
+              <GrClose
+                onClick={() => {
+                  setBarState(!barState);
+                  console.log(barState);
+                }}
+              />
+            ) : (
+              <FaBars
+                onClick={() => {
+                  setBarState(!barState);
+                  console.log(barState);
+                }}
+              />
+            )}
+          </li>
+        </ul>
+      </div>
     </div>
   );
-
 }
-
