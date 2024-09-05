a7b7c8d498361a8487afb7ff42a2d59be12b07d7
renzocabarios <renzo.cabarios@gmail.com>
2024-09-05 21:45:37 +0800

    Update

--- dapp/hooks/useCreateCandyMachine.tsx
+++ dapp/hooks/useCreateCandyMachine.tsx
@@ -2,7 +2,10 @@ import { useMutation } from "@tanstack/react-query";
 import useUmi from "@/hooks/useUmi";
 import { generateSigner, some } from "@metaplex-foundation/umi";
 import { createCollectionV1 } from "@metaplex-foundation/mpl-core";
-import { create } from "@metaplex-foundation/mpl-core-candy-machine";
+import {
+  addConfigLines,
+  create,
+} from "@metaplex-foundation/mpl-core-candy-machine";
 
 export default function useCreateCandyMachine() {
   const umi = useUmi();
@@ -33,6 +36,23 @@ export default function useCreateCandyMachine() {
                 uriLength: 29,
                 isSequential: false,
               }),
+            })
+          )
+          .add(
+            await addConfigLines(umi, {
+              candyMachine: candyMachine.publicKey,
+              index: 0,
+              configLines: [
+                { name: "1", uri: "nft1.json" },
+                { name: "2", uri: "nft2.json" },
+                { name: "3", uri: "nft3.json" },
+                { name: "4", uri: "nft4.json" },
+                { name: "5", uri: "nft5.json" },
+                { name: "6", uri: "nft6.json" },
+                { name: "7", uri: "nft7.json" },
+                { name: "8", uri: "nft8.json" },
+                { name: "9", uri: "nft9.json" },
+              ],
             })
           )
           .sendAndConfirm(umi);
